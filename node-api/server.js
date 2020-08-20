f6d3c2b26f76a5c4d2e235f96879d94af1aba006
tadeutr <tadeu.tripoli@gmail.com>
2020-08-19 21:54:16 -0300

    finalizaçao da API

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
+// inciando o app
 const app = express();
+app.use(express.json());
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello RocketSeat');
-});
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+require('./src/models/Product');
+//requireDir('./src/models')   - necessário instalar npm require-dir
+
+// rotas
+app.use('/api', require('./src/routes'));
+
+
 
 app.listen(3001)
