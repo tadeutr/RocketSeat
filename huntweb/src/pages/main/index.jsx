5170a2737abc77ae1e30566e174eb666a727a347
tadeutr <tadeu.tripoli@gmail.com>
2020-09-13 20:21:36 -0300

    final

--- huntweb/src/pages/main/index.jsx
+++ huntweb/src/pages/main/index.jsx
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
-import api from '../../services/api'
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 
-export class Main extends Component {
-    //variavel (state) armazena a informação do backend
-    state = {
-        products: [],
+import './styles.css'
 
-    }
+function Main() {
+    //cria um array vazio de products
+    const [products, setproducts] = useState([]);
+    //cria um objeto vazio para as informações da pagina de produtos
+    const [productInfo, setproductInfo] = useState({});
+    const [page, setPage] = useState(1)
 
-    componentDidMount() {
-        this.loadProducts();
-    }
-    //função para carregar as informações do backend
-    loadProducts = async () => {
-        const response = await api.get('/products');
+    //insere no array os dados que foram carregados acima
+      
+    
+    useEffect(async(page = 1) => { 
+        const response = await api.get(`/products?page${page}`) ;
 
-        this.setState({ products: response.data.docs})
-}
+        const {docs, ...productInfo} = response.data;
+        
+    //carrega do backend os dados para inserir no array
 
-render() {
+        setproducts(response.data.docs);
+        setproductInfo(response.data.productInfo);
+               
+    }, []);
+
+    function nextPage () {
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+    };
+
+    //mosta na tela (componente) o array carregado (map)
     return (
         <div className="product-list">
-            {this.state.products.map(product => (
-                <h2 key={product._id}>{product.title}</h2>
+            {products.map(product => (
+                <article key={product._id}>
+                    <strong>{product.title}</strong>
+                    <p>{product.description}</p>
+
+                    <a href="">Acessar</a>
+                </article>
             ))}
+            <div className="actions">
+                <button >Anterior</button>
+                <button onClick={nextPage}>Próxima</button>
+            </div>
         </div>
-    )
-}
+    );
 }
 
-export default Main
+export default Main;
